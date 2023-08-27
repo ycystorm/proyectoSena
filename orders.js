f295aa55d7fde7c81974f8cd5b386fa371333db1
ycystorm <jeancarloquirogahernandez958@gmail.com>
2023-08-27 17:49:44 -0500

    modulo empleado

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Daniel',
+        productNumber: 'perez',
+        paymentStatus: 'daniel@gmail.com',
+        status: 'dig 60 c sur '
+    },
+    {
+        productName: 'Velandia',
+        productNumber: 'paez',
+        paymentStatus: 'velandia@gmail',
+        status: 'dig 70 c sur'
+    },
+    {
+        productName: 'Aiko',
+        productNumber: 'maron',
+        paymentStatus: 'aiko@gmail.com',
+        status: 'dig 80 b sur'
+    },
+]
